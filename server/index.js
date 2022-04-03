d62e77da1e436ba4865c562db0519214d9d6fdbf
Andrey <1037cs@gmail.com>
2022-04-03 14:07:56 +0400

    clearing front and init server

--- server/index.js
+++ server/index.js
@@ -1,18 +1,21 @@
 const express = require('express')
 const config = require('config')
-const mongoose = require('mongoose');
-
+const mongoose = require('mongoose')
+//
 const app = express()
-const PORT = config.get('port') || 8080
-const user = require('../models/user')
 
-async function start() {
-    try {
-        await mongoose.connect(config.get('mongooseUrl'))
-        console.log('database connected')
-    } catch (e) {
-        //TODO Обработать правильно ошибку.
-    }
+const PORT = config.get('port') || 8080 //get port from '/config/default.json'
+// const user = require('../models/user')
+
+const start = async () => {
+	try{
+		await mongoose.connect(config.get('databaseURL'))
+		app.listen(PORT,()=>{
+			console.log(`Server is working on ${PORT}...`)
+		})
+	} catch (e) {
+
+	}
 }
 
 start()
