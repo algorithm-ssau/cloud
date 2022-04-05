277322fbf11bf7af7e9f6939b71068a874837974
Andrey <1037cs@gmail.com>
2022-04-05 12:03:56 +0400

    installed express-validation

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,8 +1,9 @@
 const Router = require('express')
 const User = require('../models/user.js')
+const bcrypt = require('bcryptjs')
 const router = new Router()
 
-router.post('/registration', (req, res) => {
+router.post('/registration', async (req, res) => {
 	try {
 		const {email,password} = req.body
 		const candidate = User.findOne({email})
@@ -11,8 +12,10 @@ router.post('/registration', (req, res) => {
 			res.status(400).json({message:'User with this email is already exists'})
 		}
 
-		const user = new User({email})
-
+		const hashPassword = await bcrypt(password, 15)
+		const user = new User({email,password: hashPassword})
+		await user.save()
+		return res.send({message:'User was succesfully created '})
 	} catch (e) {
 		console.log(e)
 		res.send({message: 'Server error'})
