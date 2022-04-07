e88548ac6849015c1018ff4ec2f53361bbd20115
Andrey <1037cs@gmail.com>
2022-04-07 13:44:01 +0400

    editing

--- server/models/user.js
+++ server/models/user.js
@@ -4,11 +4,8 @@ const User = new Schema ({
 	email: {type: String, required:true,unique:true},
 	password: {type: String, required: true},
 	diskSpace: {type: Number, default:1024**3*10},
-<<<<<<< HEAD
 	usedSpace: {type: Number, default: 0},
-=======
 	used: {type: Number, default: 0},
->>>>>>> 00620b19df43214b588c3027fb7c3ab2ecdf56f3
 	avatar: {type: String},
 	files:[{type: ObjectId, ref:'File'}]
 })
