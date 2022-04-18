b89f7a8470973bf23c1801cc10c56a0abf095f3a
Andrey <1037cs@gmail.com>
2022-04-18 23:52:34 +0400

    install redux-dev-tools

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,10 +1,11 @@
 import {combineReducers, createStore} from "redux";
 import {userReducer} from "./userReducer";
 import {fileReducer} from "./fileReducer";
+import {composeWithDevTools} from "redux-devtools-extension";
 
 const rootReducer = combineReducers({
 	user: userReducer,
 	files: fileReducer
 })
 
-export const store = createStore(rootReducer)
+export const store = createStore(rootReducer,composeWithDevTools)
