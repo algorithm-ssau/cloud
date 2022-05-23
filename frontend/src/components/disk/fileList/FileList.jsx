40a8b1119c89dfaa081f55040ea4104da6ae2208
Andrey <1037cs@gmail.com>
2022-05-23 21:45:46 +0400

    add folder animation

--- frontend/src/components/disk/fileList/FileList.jsx
+++ frontend/src/components/disk/fileList/FileList.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {useSelector} from "react-redux";
 import File from "./file/File";
+import {CSSTransition, TransitionGroup} from "react-transition-group";
+
 import ('./fileList.scss')
 
 const FileList = () => {
 
-	const files = useSelector(state => state.files.files).map(file => <File key={file._id} file={file}/>)
+	const files = useSelector(state => state.files.files)
 
 	return (
 		<div className='fileList'>
@@ -14,7 +16,18 @@ const FileList = () => {
 				<div className="fileList_date">Date</div>
 				<div className="fileList_size">Size</div>
 			</div>
-			{files}
+			<TransitionGroup>
+				{files.map(file =>
+					<CSSTransition
+						key={file._id}
+						timeout={500}
+						classNames={'file'}
+						exit={false}
+					>
+						<File file={file}/>
+					</CSSTransition>
+				)}
+			</TransitionGroup>
 		</div>
 	);
 };
